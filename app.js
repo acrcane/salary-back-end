094a28c6f6f0bbc3320832c888d0d8412f41bb06
arcane <arcanesir.neo@gmail.com>
2025-11-13 12:55:28 +0100

    add: options

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
-import dotenv from "dotenv";
-import connectDB from "./db/connect.js";
-import userRouter from "./routes/usersRouter.js";
-import tableRoute from "./routes/tablesRouter.js";
-import workSessionRouter from "./routes/workSessionsRouter.js";
-import managerRoute from "./routes/managerRoute.js";
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './db/connect.js';
+import userRouter from './routes/usersRouter.js';
+import tableRoute from './routes/tablesRouter.js';
+import workSessionRouter from './routes/workSessionsRouter.js';
+import managerRoute from './routes/managerRoute.js';
 
 dotenv.config();
 const { PORT } = process.env;
 
 const app = express();
 
-const allowedOrigins = [
-  "http://localhost:5173",       
-  "https://acrcane.github.io",  
-];
+const allowedOrigins = ['http://localhost:5173', 'https://acrcane.github.io'];
 
-app.use(morgan("tiny"));
-app.use(cors({
-  origin: function(origin, callback){
-    if(!origin || allowedOrigins.includes(origin)){
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'))
-    }
-  },
-  credentials: true
-}));
+app.use(morgan('tiny'));
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
+app.options(
+  '*',
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.use("/users", userRouter);
-app.use("/table", tableRoute)
-app.use('/work-session', workSessionRouter)
-app.use('/manager', managerRoute)
+app.use('/users', userRouter);
+app.use('/table', tableRoute);
+app.use('/work-session', workSessionRouter);
+app.use('/manager', managerRoute);
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
+  res.status(404).json({ message: 'Route not found' });
 });
 
 app.use((err, req, res, next) => {
-  console.error(err.message)
-  const { status = 500, message = "Server error" } = err;
+  console.error(err.message);
+  const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
 });
 

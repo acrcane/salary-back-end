ed481168e45f4d81fb6c488758ff217671993fc9
arcane <arcanesir.neo@gmail.com>
2026-03-21 11:46:08 +0100

    update manager logic. add getLastTable

--- controllers/managerControllers.js
+++ controllers/managerControllers.js
@@ -1,17 +1,33 @@
-import HttpError from "../helpers/HttpError.js";
-import { getAllManagerService } from "../services/managerService.js";
+import HttpError from '../helpers/HttpError.js';
+import {
+  getAllManagerService,
+  getUserLastTable,
+} from '../services/managerService.js';
 
 export const getAllUsesTable = async (req, res, next) => {
-    try {
-        const user = req.user
-        if(user.role !== 'manager'){
-            throw HttpError(401, 'No access')
-        }
-        const tables = await getAllManagerService()
-        console.log(tables);
-        
-        res.status(200).json(tables)
-    } catch (error) {
-        next(error)
+  try {
+    const user = req.user;
+    if (user.role !== 'manager') {
+      throw HttpError(401, 'No access');
     }
-}
+
+    const tables = await getAllManagerService();
+
+    res.status(200).json(tables);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getLastTableController = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const table = await getUserLastTable(id);
+    if (!table) {
+      throw HttpError(404, 'not found');
+    }
+    res.status(200).json(table);
+  } catch (error) {
+    next(error);
+  }
+};
--- services/managerService.js
+++ services/managerService.js
@@ -1,6 +1,14 @@
-import { Table } from "../db/models/Tabel.js";
+import { Table } from '../db/models/Tabel.js';
 
 export const getAllManagerService = async () => {
-    const tables = await Table.find({status: 'open'}).populate('owner')
-    return tables
-}
+  const tables = await Table.find({ status: 'open' }).populate('owner');
+  return tables;
+};
+
+export const getUserLastTable = async id => {
+  const table = await Table.findOne({ owner: id, status: 'close' })
+    .sort({ _id: -1 })
+    .populate('workSession')
+    .lean();
+  return table;
+};

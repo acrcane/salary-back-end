9717b475e2f19ad3a06bf6dc140a12cc6ad59060
arcane <arcanesir.neo@gmail.com>
2026-04-26 13:16:12 +0200

    fix: remove try catch

--- middlewares/isManager.js
+++ middlewares/isManager.js
@@ -1,13 +1,11 @@
 import HttpError from '../helpers/HttpError.js';
 
-export const isManager = async (req, res, next) => {
-  try {
-    if (!req.user) throw HttpError(401, 'no authorization');
-    if (req.user.role !== 'manager') {
-      throw HttpError(403, 'Manager only');
-    }
-    next();
-  } catch (error) {
-    next(error);
+export const isManager = (req, _, next) => {
+  if (!req.user || !req.user.role) {
+    throw HttpError(401, 'no authorization');
   }
+  if (req.user.role !== 'manager') {
+    throw HttpError(403, 'Manager only');
+  }
+  next();
 };

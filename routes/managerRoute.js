4587422eabca26b5f16649f969ed3e14817144be
arcane <arcanesir.neo@gmail.com>
2026-06-22 12:31:04 +0200

    add: schedule & manager routes

--- routes/managerRoute.js
+++ routes/managerRoute.js
@@ -3,6 +3,8 @@ import { isValidToken } from '../middlewares/isValidToken.js';
 import { isManager } from '../middlewares/isManager.js';
 import { currentUserController, getAllUsersController } from '../controllers/usersControllers.js';
 import { getAllUsesTable, getLastTableController, removeUserController, updateUserController } from '../controllers/managerControllers.js';
+import validateBody from '../helpers/validateBody.js';
+import { updateUserSchema } from '../schemas/managerSchema.js';
 
 const managerRoute = express.Router()
 
@@ -10,7 +12,7 @@ managerRoute.get('/',isValidToken, isManager, currentUserController);
 managerRoute.get('/users', isValidToken, isManager, getAllUsersController )
 managerRoute.get('/tables', isValidToken, isManager, getAllUsesTable)
 managerRoute.get('/users/:id/last-table', isValidToken, isManager, getLastTableController)
-managerRoute.patch('/update/:id', isValidToken, isManager, updateUserController)
+managerRoute.patch('/update/:id', isValidToken, isManager, validateBody(updateUserSchema), updateUserController)
 managerRoute.delete('/user/:id', isValidToken, isManager, removeUserController)
 
 export default managerRoute
--- routes/scheduleRoute.js
+++ routes/scheduleRoute.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { isValidToken } from '../middlewares/isValidToken.js';
 import { isManager } from '../middlewares/isManager.js';
 import validateBody from '../helpers/validateBody.js';
-import {  createSchedule } from '../controllers/scheduleControllers.js';
+import {  createSchedule, getActiveScheduleContriller } from '../controllers/scheduleControllers.js';
 import { scheduleSchema } from '../schemas/scheduleSchema.js'
 import { createSheduleDayController } from '../controllers/scheduleDaysController.js';
 import { addShiftController } from '../controllers/scheduleShiftsController.js';
@@ -10,9 +10,8 @@ import { addShiftController } from '../controllers/scheduleShiftsController.js';
 const scheduleRoute = express.Router()
 
 scheduleRoute.post('/', validateBody(scheduleSchema),isValidToken, isManager, createSchedule)
-
 scheduleRoute.post('/:scheduleId/days', isValidToken, isManager, createSheduleDayController)
-
 scheduleRoute.post('/:scheduleId/shifts', isValidToken, isManager, addShiftController)
+scheduleRoute.get('/active', isValidToken, getActiveScheduleContriller)
 
 export default scheduleRoute
